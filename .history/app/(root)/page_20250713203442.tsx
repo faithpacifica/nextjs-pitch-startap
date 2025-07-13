00496160c3e9afc2939854a889a9ed2ce2700e56
Dildora Muslimova <faithpacifica@yahoo.com>
2025-07-13 22:12:47 +0900

    feat:update global.css file install shadcn

---
+++ .history/app/(root)/page_20250713203442.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1 className="heading">Pitch Your Startup, 
+Connect with Entrepreneurs</h1>
+    </>
+  );
+}

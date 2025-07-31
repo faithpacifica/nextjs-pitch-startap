e7ecc1c219bfa63ae0813f30ff4902d5a89542d1
Dildora Muslimova <faithpacifica@yahoo.com>
2025-08-01 02:07:31 +0900

    fix:ts type errors

--- auth.ts
+++ auth.ts
@@ -12,12 +12,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 			clientSecret: process.env.AUTH_GITHUB_SECRET!,
 		}),
 	],
+  //after successful athentication:
 	callbacks: {
-		// to check wether our user already exists in our DB or not and then depending on that created
-		async signIn({
-			user: { name, email, image },
-			profile: { id, login, bio },
-		}) {
+		// to check wether our user already exists in our DB or not and then depending on that to create
+		async signIn({ user: { name, email, image }, profile }) {
+			if (!profile) return false;
+			const { id, login, bio } = profile as { id: string; login: string; bio?: string };
+
 			const existingUser = await client
 				.withConfig({ useCdn: false })
 				.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {

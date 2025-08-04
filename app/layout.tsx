61bfb25e3ab5a97ed2b4ea038811e1e0b6547488
Dildora Muslimova <faithpacifica@yahoo.com>
2025-08-04 22:23:25 +0900

    create startup for validation function, add toast logic and ui,fix type errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from 'next/font/local'
 import "./globals.css";
 import "easymde/dist/easymde.min.css"; // for markdown editor styles
-// import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/sonner"
 
 const workSans = localFont({
   src:[
@@ -57,6 +57,7 @@ export default function RootLayout({
       <body
         className={workSans.variable}>
         {children}
+        {<Toaster />}
       </body>
     </html>
   );
